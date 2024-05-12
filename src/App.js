e196d381ef828114af6fb321bf9a4027741b7616
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-05-11 21:40:54 -0400

    Create balance for friends

--- src/App.js
+++ src/App.js
@@ -42,5 +42,24 @@ function FriendList() {
 }
 
 function Friend({ friend }) {
-    return <li>{friend.name}</li>;
+    return (
+        <li>
+            <img src={friend.image} alt={friend.name} />
+            <h3>{friend.name}</h3>
+
+            {friend.balance < 0 && (
+                <p className="red">
+                    You owe{friend.name} {Math.abs(friend.balance)}€
+                </p>
+            )}
+
+            {friend.balance > 0 && (
+                <p className="green">
+                    {friend.name} owes you {Math.abs(friend.balance)}€
+                </p>
+            )}
+
+            {friend.balance < 0 && <p>You and{friend.name} are even </p>}
+        </li>
+    );
 }
